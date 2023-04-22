e95803521b92111fdf999fb5ed504a0d3e88903a
viveksura <emailviveksura@gmail.com>
2023-04-22 00:44:40 -0500

    fix issues with docker

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,6 +67,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  ENV['OPEN_AI_API_KEY'] = ""
+
   ENV['OPEN_AI_LLM'] = "text-embedding-ada-002"
 end
